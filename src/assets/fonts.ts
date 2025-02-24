34e5f55d363345c5c6d393b82bd54ca3b5cafc1b
sappiah085 <sappiah5689@gmail.com>
2025-02-24 18:24:03 +0100

    added navigation bar

--- src/assets/fonts.ts
+++ src/assets/fonts.ts
@@ -1,4 +1,27 @@
 import { Karla, JetBrains_Mono, Inter } from "next/font/google";
-export const karla_font = Karla({ variable: "--karla" });
-export const jetBrains_font = JetBrains_Mono({ variable: "--jet-brains" });
-export const inter_font = Inter({ variable: "--jet-brains" });
+export const karla_font = Karla({
+	variable: "--karla",
+	subsets: ["latin", "latin-ext"],
+});
+export const jetBrains_font = JetBrains_Mono({
+	variable: "--jet-brains",
+	subsets: [
+		"cyrillic",
+		"cyrillic-ext",
+		"greek",
+		"latin",
+		"latin-ext",
+		"vietnamese",
+	],
+});
+export const inter_font = Inter({
+	variable: "--jet-brains",
+	subsets: [
+		"cyrillic",
+		"cyrillic-ext",
+		"greek",
+		"latin",
+		"latin-ext",
+		"vietnamese",
+	],
+});
